22a24e1aefce803a5d48862888507faf391e8506
Максим <98467035+7LeoFront7@users.noreply.github.com>
2023-08-27 21:12:23 +0300

    Add files via upload

--- src/assets/pages/screencast.jsx
+++ src/assets/pages/screencast.jsx
@@ -1,11 +1,12 @@
 import style from './screencast.module.css'
 
+
 function Screencastpage() {
     return (  
         <div className={style.screencast}>
         
         <div className={style.screencast__right}>
-        <video controls='true' src="./2.mp4"></video>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/87wTaJSVABU?si=lobPEJjHP5THV7Uc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         <div className={style.screencast__left}>
         <h1>Делюсь с Вами <span>интересной фишкой</span> из <span>CSS!</span></h1>
