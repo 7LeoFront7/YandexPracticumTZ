b0b68640eb970b6a29ef459f754e59d42a645f3d
Максим <98467035+7LeoFront7@users.noreply.github.com>
2023-08-27 16:24:32 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './assets/components/header/Header'
+import {Gallerypage} from './assets/pages/gallery'
 
 import './global.css'
 
@@ -10,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     
     <div className='wrapper'>
        <Header />
-       
+        <Gallerypage/>
     </div>
   
   </React.StrictMode>
